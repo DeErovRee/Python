a6b1ae7b69dffff8f8f855292aa14b8b00547610
Oksana TUF <den_nesterov_1992@mail.ru>
2023-09-19 14:37:17 +0500

    Python-2

---
+++ 10.py
@@ -0,0 +1,14 @@
+from random import randint
+
+n = int(input('Введите число монет: '))
+a, b = 0, 0
+for i in range(n): 
+    moneta = randint(0,1)
+    print(moneta, end=" ")
+    if moneta > 0:
+        a += 1
+    else: b += 1
+print()
+if a > b: 
+    print(f'Нужно перевернуть {b} монеток')
+else: print(f'Нужно перевернуть {a} монеток')
